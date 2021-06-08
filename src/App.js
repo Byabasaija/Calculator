9677b13a7172cfc2855b6da034f9bac3cc606f43
Byabasaija <basaijapascal9@gmail.com>
2021-06-09 02:18:47 +0300

    finish set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import PropTypes from 'prop-types'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1> Calculator</h1>
+      <p>Welcome to my calculator</p>
+     </div>
   );
 }
 
+App.propTypes = {
+  name : PropTypes.string
+}
 export default App;
