2066a9757e886e85761c8ad95820888c688cd654
Byabasaija <basaijapascal9@gmail.com>
2021-06-10 18:58:21 +0300

    Update App to use fragments

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,10 @@ import Display from './Display';
 import ButtonPannel from './ButtonPannel';
 
 const App = () => (
-  <div className="App">
+  <>
     <Display />
     <ButtonPannel />
-  </div>
+  </>
 );
 
 export default App;
