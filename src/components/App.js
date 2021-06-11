91a493c996b015172b317f327a6e976dbc0bbe7f
Byabasaija <basaijapascal9@gmail.com>
2021-06-11 16:23:47 +0300

    implement calculate and operate modules

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPannel from './ButtonPannel';
+import calculate from '../logic/calculate';
+
+calculate({ total: '10', next: '5', operation: 'X' }, '=');
 
 const App = () => (
   <>
