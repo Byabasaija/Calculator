52fb17ad991524ce9a77f36c57cfe436592c2693
Byabasaija <basaijapascal9@gmail.com>
2021-06-18 14:03:39 +0300

    clean code

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './components/App';
-import Header from './components/Navbar';
+import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Quote from './components/Quote';
 
 const Routes = () => (
   <BrowserRouter>
-    <Header />
+    <Navbar />
     <Switch>
       <Route path="/" component={Home} exact />
       <Route path="/app" component={App} />
